c4166d430a8a0ed22a6a490c6d707b5b13fe1a6e
CodesAndBhargavi <bhargvainaik41439@gmail.com>
2026-08-27 13:02:14 +0530

    Restructure with Day 1 folder

---
+++ Day 1/p1.java
@@ -0,0 +1,16 @@
+//variables in java
+public class p1 {
+    public static void main(String[] args) {
+        short shortnumber = 32000;
+        int age = 25;
+
+        float temperature = 36.6f;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isEnrolled = true;
+
+        String name = "Bhargavi";
+        System.out.println("Student : " + name);
+    }
+    
+}
